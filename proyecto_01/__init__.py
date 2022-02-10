23efb852bb7be07bcafdc37aef532a41a0bed9e8
ccmorenom <camilo@theblackpuma.com>
2022-02-10 04:46:39 -0300

    add uuid, folder images and contest model

--- proyecto_01/__init__.py
+++ proyecto_01/__init__.py
@@ -7,8 +7,6 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-
-    app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
     app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/proyecto_01.db'
 
     db.init_app(app)
--- proyecto_01/main.py
+++ proyecto_01/main.py
@@ -1,5 +1,9 @@
 from flask_login import current_user
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+import uuid
+from datetime import datetime
+from .models import Contest, Users
+
 from werkzeug.utils import secure_filename
 
 from . import db
@@ -18,14 +22,25 @@ def create_contest():
     return render_template('create_contest.html')
 @main.route('/create_contest', methods=['POST'])
 def create_contest_post():
-    name_contest = request.form.get('namecontest')
-    date_start = request.form.get('datestart')
-    date_end = request.form.get('dateend')
+    contest_name = request.form.get('namecontest')
+    start_date = request.form.get('datestart')
+    end_date = request.form.get('dateend')
     award = request.form.get('award')
     dialog = request.form.get('dialog')
     description = request.form.get('description')
     banner = request.files['file']
-    banner.save('mio.png')
-    #path_banner =  'path' + 'name'
-
-    return 'ssas'
+    filename = str(uuid.uuid1()) + '.' + banner.filename.split('.')[-1]
+    banner.save( '/home/camilo/Documents/cloud/proyecto_01/uploads/images/' + filename)
+    new_contest = Contest(id_user = current_user.id,
+                            contest_name = contest_name,
+                            banner_name=filename,
+                            url_contest='192.168.0.1:8080/abcd',
+                            start_date=datetime.strptime(start_date,format='%Y-%m-%d'),
+                            end_date=datetime.strptime(end_date,format='%Y-%m-%d'),
+                            award=float(award),
+                            dialog=dialog,
+                            description=description)
+    db.session.add(new_contest)
+    db.session.commit()
+    flash('evento cargado.')
+    return redirect(url_for('main.index'))
