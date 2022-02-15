38fb040329f9cdd117a30ec997ddd7c3dc73fd3d
ccmorenom <camilo@theblackpuma.com>
2022-02-14 22:57:53 -0300

    cron convert song

--- proyecto_01/convert_audio.py
+++ proyecto_01/convert_audio.py
@@ -1,22 +1,47 @@
-from pydub import AudioSegment
+import sqlite3 as sql
 import os
-path_audio = os.path.abspath(os.curdir) + '/uploads/dialog_song_convert/'
-path_audio_convert  = os.path.abspath(os.curdir) + '/uploads/dialog_song_convert/'
+from pydub import AudioSegment
 
 
-1. consultar en la base de datos de propust todos los adios que aun no han sido convertidos
-2. traer todos los nombres y  buscarlos en el path
-3. pasar cada unos de los audios en la funcion
-4. validar que los audios fueron convertidos
-5. hacer un update de los audios que fueron convertidso en la tabla proposal.
+path = os.path.abspath(os.curdir)
+path_input = path + '/uploads/dialog_song/'
+path_output = path + '/uploads/dialog_song_convert/'
 
+print(path)
+sqlconnection = sql.connect(path + '/database/proyecto_01.db')
+cursor = sqlconnection.cursor()
 
+query='''
+select id_proposal,
+       dialogo_sound
+from proposal
+-- where state_voice = 'in process';
+'''
+cursor.execute(query)
+records = cursor.fetchall()
+id_proposal = []
+name_file = []
+columnNames=[column[0] for column in cursor.description]
+for record in records:
+    id_proposal.append(record[0])
+    name_file.append(record[1])
 
-arr = os.listdir(path_audio)
-format= arr[0].split('.')[-1]
-name = arr[0].split('.')[0]
+name_file_search = name_file[:30]
+arr = os.listdir(path + '/uploads/dialog_song')
+##name_file_search in arr
 
-def convert_audio(input_path,out_path,format_input):
-    given_audio = AudioSegment.from_file(path_audio + arr[0], format=format)
-    given_audio.export(path_audio_convert + arr[0], format="mp3")
+def convert_audio(input_path,out_path,file):
+    format = file.split('.')[-1]
+    name_file = file.split('.')[0]
+    given_audio = AudioSegment.from_file(input_path + file, format=format)
+    given_audio.export(out_path + name_file+'.mp3', format="mp3")
 
+for file in name_file_search:
+    convert_audio(path_input, path_output, file)
+
+query_update=f'''
+UPDATE proposal
+SET state_voice = 'convert'
+WHERE id_proposal in {id_proposal};
+'''
+print(query_update)
