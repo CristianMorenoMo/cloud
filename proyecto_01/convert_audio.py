7ac6acc5f620766d15fa4f8a7fc4e215f8eb9385
ccmorenom <camilo@theblackpuma.com>
2022-02-15 15:41:26 -0300

    to convert song and to send emails script competed

--- proyecto_01/convert_audio.py
+++ proyecto_01/convert_audio.py
@@ -1,11 +1,19 @@
 import sqlite3 as sql
 import os
 from pydub import AudioSegment
+import yagmail
 
+
+def convert_audio(input_path, out_path, file):
+    format = file.split('.')[-1]
+    name_file = file.split('.')[0]
+    given_audio = AudioSegment.from_file(input_path + file, format=format)
+    given_audio.export(out_path + name_file + '.mp3', format="mp3")
+    print(f'completado:{name_file}')
 
 path = os.path.abspath(os.curdir)
-path_input = path + '/uploads/dialog_song/'
-path_output = path + '/uploads/dialog_song_convert/'
+path_input = path + '/static/uploads/dialog_song/'
+path_output = path + '/static/uploads/dialog_song_convert/'
 
 print(path)
 sqlconnection = sql.connect(path + '/database/proyecto_01.db')
@@ -13,35 +21,47 @@ cursor = sqlconnection.cursor()
 
 query='''
 select id_proposal,
-       dialogo_sound
+       dialogo_sound,
+       email
 from proposal
--- where state_voice = 'in process';
+where state_voice = 'in process';
 '''
+query_update = '''
+    UPDATE proposal
+    SET state_voice = 'convert', dialogo_sound_convert ="{name_file}", formato="mp3"
+    WHERE id_proposal = CAST("{id}" as INT);
+    '''
+
 cursor.execute(query)
 records = cursor.fetchall()
 id_proposal = []
 name_file = []
+email=[]
 columnNames=[column[0] for column in cursor.description]
 for record in records:
     id_proposal.append(record[0])
     name_file.append(record[1])
+    email.append(record[2])
 
 name_file_search = name_file[:30]
-arr = os.listdir(path + '/uploads/dialog_song')
-##name_file_search in arr
+arr = os.listdir(path + '/static/uploads/dialog_song')
 
-def convert_audio(input_path,out_path,file):
-    format = file.split('.')[-1]
-    name_file = file.split('.')[0]
-    given_audio = AudioSegment.from_file(input_path + file, format=format)
-    given_audio.export(out_path + name_file+'.mp3', format="mp3")
+if all([i in arr for i in name_file_search]):
+    i=0
+    for row in name_file_search:
+        name_file= row.split('.')[0] + '.mp3'
+        convert_audio(path_input, path_output, row)
+        query_update_f = query_update.format(name_file =name_file,
+                                             id =id_proposal[i])
+        print(query_update_f)
+        cursor.execute(query_update_f)
+        sqlconnection.commit()
+        print('update file')
+        i = i+1
 
-for file in name_file_search:
-    convert_audio(path_input, path_output, file)
-
-query_update=f'''
-UPDATE proposal
-SET state_voice = 'convert'
-WHERE id_proposal in {id_proposal};
-'''
-print(query_update)
+        #yag = yagmail.SMTP(email,password)
+        #yag.send(email[i],
+        #         'Audio convertido',
+        #         ['Audio convertido'])
+        #print('email enviado')
+        break
